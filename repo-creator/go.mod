ee6733495a343f5c54bb8ecac519594fce9dcc46
equanox <matthias@benchkram.de>
2022-11-25 16:44:29 +0100

    repo creator tool

---
+++ repo-creator/go.mod
@@ -0,0 +1,10 @@
+module repo-creator
+
+go 1.18
+
+require github.com/benchkram/errz v0.0.0-20211210135050-f997ca868855
+
+require (
+	github.com/go-logr/logr v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- repo-creator/main.go
+++ repo-creator/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"repo-creator/template"
 
+	"github.com/benchkram/errz"
+)
+
+func main() {
+	fmt.Println("Creating monorepo")
+
+	err := os.MkdirAll("test", 0755)
+	errz.Fatal(err)
+
+	err = template.CreateGoProject("./test", 1)
+	errz.Fatal(err)
 }
